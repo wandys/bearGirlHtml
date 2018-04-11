841366cb0c7a8be6ff6e999b616a43baa4c68d79
wandy <578295662@qq.com>
2018-04-11 16:44:02 +0800

    新建项目

---
+++ src/main/java/cn/beargirl/html/bearGirl/BearGirlApplication.java
@@ -0,0 +1,12 @@
+package cn.beargirl.html.bearGirl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BearGirlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BearGirlApplication.class, args);
+	}
+}
---
+++ src/test/java/cn/beargirl/html/bearGirl/BearGirlApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.beargirl.html.bearGirl;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BearGirlApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
